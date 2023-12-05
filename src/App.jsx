536aad298260cd720a67a4aae73b56f34e5fdffc
jabongg <jbpvns@gmail.com>
2023-12-05 09:09:40 +0530

    react router dom demo for a vite project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>React Router DOM</h1>
+
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
